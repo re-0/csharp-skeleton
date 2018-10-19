f43bcc11b1a5ceb9a945b73553f8d5d82c92f75e
Regi <32775617+re-0@users.noreply.github.com>
2018-10-19 21:37:22 +0100

    Update Question1.cs

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -7,11 +7,22 @@ using System.Threading.Tasks;
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question1
-    {
+    {   
         public static int Answer(int[] portfolios)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            Array.Sort(portfolios); 
+      
+            ushort maxXor = ushort.MinValue; 
+            ushort val = (ushort)0; 
+      
+            // find max xor
+            for(int i = 0; i < portfolios.Length - 1; i++)
+            {
+                val = (ushort)(portfolios[i] ^ portfolios[i + 1]); 
+                maxXor = Math.Max(maxXor, val); 
+            } 
+      
+            return maxXor; 
         }
     }
 }
